95edb4f1e951efa8395c03c79c1ccf8e501b0c40
kene <kenechiifeanyi@gmail.com>
2025-02-05 14:28:56 +0100

    clear git cache

--- README.md
+++
@@ -1,2 +0,0 @@
-# ednet-fiber-api
-A Realtime EdTech API Built With Go Fiber Framework And Ent ORM 
--- config/validators.go
+++
@@ -1,6 +0,0 @@
-package config
-
-// Validates if a account type value is the correct one
-// func AccountTypeValidator(fl validator.FieldLevel) bool {
-// 	return fl.Field().Interface().(choices.AccType).IsValid()
-// }
