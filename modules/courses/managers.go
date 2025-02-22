d16afed0e1261548a7ca105d17ccd6ea3acaced3
kene <kenechiifeanyi@gmail.com>
2025-02-22 13:37:56 +0100

    managers: courses and categories

--- modules/courses/managers.go
+++ modules/courses/managers.go
@@ -12,8 +12,18 @@ import (
 
 type CourseManager struct{}
 
+func (c CourseManager) GetCategories(db *ent.Client, ctx context.Context) []*ent.Category {
+	categories := db.Category.Query().AllX(ctx)
+	return categories
+}
+
 func (c CourseManager) GetCategoryByName(db *ent.Client, ctx context.Context, name string) *ent.Category {
 	category, _ := db.Category.Query().Where(category.NameEQ(name)).Only(ctx)
+	return category
+}
+
+func (c CourseManager) GetCategoryBySlug(db *ent.Client, ctx context.Context, slug string) *ent.Category {
+	category, _ := db.Category.Query().Where(category.SlugEQ(slug)).Only(ctx)
 	return category
 }
 
@@ -30,5 +40,22 @@ func (c CourseManager) GetAll(db *ent.Client, fibCtx *fiber.Ctx) *config.Paginat
 
 func (c CourseManager) GetCourseByName(db *ent.Client, ctx context.Context, name string) *ent.Course {
 	course, _ := db.Course.Query().Where(course.TitleEQ(name)).First(ctx)
+	return course
+}
+
+func (c CourseManager) FilterCoursesByName(db *ent.Client, fibCtx *fiber.Ctx, name string) *config.PaginationResponse[*ent.Course] {
+	courses := config.PaginateModel(
+		fibCtx,
+		db.Course.Query().
+			Where(course.TitleContains(name)).
+			WithInstructor().
+			WithCategory().
+			WithTags(),
+	)
+	return courses
+}
+
+func (c CourseManager) GetCourseBySlug(db *ent.Client, ctx context.Context, slug string) *ent.Course {
+	course, _ := db.Course.Query().Where(course.SlugEQ(slug)).Only(ctx)
 	return course
 }
