3e0a3edea1ba1684bff8d5facebb49525b51c1bd
kene <kenechiifeanyi@gmail.com>
2025-02-28 15:22:00 +0100

    chore: course routes

---
+++ modules/courses/routes.go
@@ -0,0 +1,12 @@
+package courses
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/kayprogrammer/ednet-fiber-api/ent"
+)
+
+func GetLatestCourses(db *ent.Client) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+
+	}
+}
